6f493a9de1d5b4c7f99e926f60f9c776bd016894
Jnana Senapati <jnanars@gmail.com>
2023-01-06 13:56:46 -0800

    Changes to deploy to production server

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     //const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5500', {
+    const response = await fetch('https://codex-0klq.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
